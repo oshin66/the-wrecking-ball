b9698b7e7e973d6ee40db902952833c69604a034
oshin66 <74353188+oshin66@users.noreply.github.com>
2021-03-02 19:51:18 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,27 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.2,
+            length: 240
+        }
+        this.pointB = pointB
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+    fly(){
+        this.rope.bodyA = null;
+    }
+
+    display(){
+        if(this.rope.bodyA){
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
